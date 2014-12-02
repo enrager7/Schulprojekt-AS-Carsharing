9bae9f256b09195f44b41cfa8f70d8cf35ad1f29
MarkusHoefgen <markus.hoefgen@web.de>
2014-12-02 21:09:22 +0100

    Database Try Catch wieder einkommentiert

--- Carsharing2/WindowsFormsApplication1/Database.cs
+++ Carsharing2/WindowsFormsApplication1/Database.cs
@@ -267,8 +267,8 @@ namespace Carsharing
         //Auto erstellen
         public void CreateCar(string licenseTag, string model, string manufacturer, decimal pricePerDay)
         {
-            //try
-            //{
+            try
+            {
                 command = new SQLiteCommand(this.connection);
                 command.CommandText = "INSERT INTO T_Car (p_licenseTag, model, manufacturer, priceperDay) VALUES('" + licenseTag + "','" + model + "','" + manufacturer + "','" + pricePerDay+"');'";
                 if (connection.State == ConnectionState.Closed)
@@ -276,11 +276,11 @@ namespace Carsharing
                 command.ExecuteNonQuery();
                 connection.Close();
                 command.Dispose();
-            //}
-            //catch (SQLiteException exception)
-            //{
-            //    throw exception;
-            //}
+            }
+            catch (SQLiteException exception)
+            {
+                throw exception;
+            }
         }
 
         //Auto löschen 
